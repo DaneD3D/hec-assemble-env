5ca56e181778c9e47bc33924f7e92fde940c7854
Dane_D <dane.danovskis@fsastore.com>
2025-08-29 15:20:04 -0400

    Bump version to 1.0.2 and add Rollup configuration for JSON plugin

---
+++ rollup.config.mjs
@@ -0,0 +1,23 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import json from '@rollup/plugin-json';
+
+export default {
+  input: 'index.mjs',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+    sourcemap: true,
+    inlineDynamicImports: true
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    json(),
+    terser()
+  ],
+  external: [
+    'fs', 'path', 'os', 'process', 'child_process'
+  ]
+};

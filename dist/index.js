092f481414db5be80069d03bc1ef6289f2817fec
Dane_D <dane.danovskis@fsastore.com>
2025-08-29 14:44:15 -0400

    Add new build

---
+++ dist/index.js
@@ -0,0 +1,2 @@
+import"@azure/identity";import"@azure/keyvault-secrets";import"@microsoft/microsoft-graph-client";import"inquirer";import"fs";import"isomorphic-fetch";export{g as getAzureCredentials,a as getCurrentUserEmail,p as promptKeyVaultSecretsAndWriteEnv,u as updateEnvFileInteractively}from"./index-c986bc4f.js";
+//# sourceMappingURL=index.js.map
